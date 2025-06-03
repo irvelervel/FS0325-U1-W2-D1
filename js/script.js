683979592cbc2978354f0b5d69afab7e031e5448
Stefano Casasola <ste.casasola@gmail.com>
2025-06-03 09:53:19 +0200

    Adds separate JS file

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// FILE JS ESTERNO, che abbiamo incorporato in index.html
+
+// perchè siamo qui oggi?
+// la professione dello sviluppatore web riguarda la RISOLUZIONE DEI PROBLEMI.
+// ARRIVARE DA UN PUNTO A (problema) AD UN PUNTO Z (soluzione)
+
+// la risoluzione di un problema si basa sulla stesura di un ALGORITMO
+// un algoritmo è una serie ordinata di passaggi che se eseguita correttamente
+// mi risolve un problema specifico. Seguendo SEMPRE gli stessi passi, arriverò
+// SEMPRE allo stesso risultato!
+
+// Nella risoluzione di un problema, spesso si ha a che fare con risultati "intermedi",
+// così come nella creazione di una torta servono delle "bacinelle" per mischiare gli
+// ingredienti tra di loro al fine poi di utilizzarli per raggiungere la meta finale.
